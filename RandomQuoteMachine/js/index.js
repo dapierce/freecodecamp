9d0576a30dda95c5e270defffd1976e2d68d99e5
David Pierce <pierceda@gmail.com>
2016-09-19 20:31:37 -0700

    Updated to use forismatic quote API.

--- RandomQuoteMachine/js/index.js
+++ RandomQuoteMachine/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   var currentQuote = -1;
-  var numOfQuotes = 2;
+  var numOfQuotes = 457653;
 
   // grabs a new quote from a JSON file, avoid current quote
   function newQuote() {
@@ -12,17 +12,14 @@ $(document).ready(function() {
     currentQuote = quoteId;
     var json = [];
     // do fancy JSON stuff here
-    $.getJSON("https://dapierce.github.io/freecodecamp/RandomQuoteMachine/quotes.json", function(json) {
-      // find the quote at quoteId
-      json = json.filter(function(val) {
-        return val.id === quoteId;
-      });
-
-      // edit HTML with quote data
-      json.forEach(function(val) {
-        $(".quote").html(val.quote);
-        $(".name").html(val.name);
-      });
+    //$.getJSON("https://dapierce.github.io/my-freecodecamp/RandomQuoteMachine/quotes.json", function(json) {
+    //// find the quote at quoteId
+      // json = json.filter(function(val) {
+      //   return val.id === quoteId;
+      // });
+     $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&key=" + currentQuote + "&format=json&lang=en", function(json) {
+        $(".quote").html(json.quoteText);
+        $(".name").html(json.quoteAuthor);
     });
   }
 
