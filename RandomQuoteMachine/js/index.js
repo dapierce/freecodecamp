40c97470e716d103d25d0f51ce500967a8d71f47
David Pierce <pierceda@gmail.com>
2016-09-23 17:40:13 -0700

    Fully functional, with tweeting

--- RandomQuoteMachine/js/index.js
+++ RandomQuoteMachine/js/index.js
@@ -1,26 +1,45 @@
 $(document).ready(function() {
-
+  // global variables
+  var TWEET_CHAR_LIMIT = 140;
+  var NUM_OF_QUOTES = 20000;
   var currentQuote = -1;
-  var numOfQuotes = 457653;
+  var quoteText = "";
+  var quoteAuthor = "";
 
-  // grabs a new quote from a JSON file, avoid current quote
+  // grabs a new quote from a JSON file, compares to avoid current quote
   function newQuote() {
     var quoteId = currentQuote;
     while (quoteId === currentQuote) {
-      quoteId = Math.floor(Math.random() * (numOfQuotes + 1));
+      quoteId = Math.floor(Math.random() * (NUM_OF_QUOTES + 1));
     }
     currentQuote = quoteId;
     var json = [];
-    // do fancy JSON stuff here
-    //$.getJSON("https://dapierce.github.io/my-freecodecamp/RandomQuoteMachine/quotes.json", function(json) {
-    //// find the quote at quoteId
-      // json = json.filter(function(val) {
-      //   return val.id === quoteId;
-      // });
-     $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&key=" + currentQuote + "&format=json&lang=en", function(json) {
-        $(".quote").html(json.quoteText);
-        $(".name").html(json.quoteAuthor);
+    $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&key=" + currentQuote + "&format=json&lang=en", function(json) {
+      // remove all Donald quotes--probably actually by his ghostwriter anyway
+      if (json.quoteAuthor == "Donald Trump") {
+        newQuote();
+      }
+      // make it elegant if no one is attributted
+      if (json.quoteAuthor === "") {
+        json.quoteAuthor = "Unattributted";
+      }
+      quoteText = json.quoteText;
+      quoteAuthor = json.quoteAuthor;
+      $(".quote").html(json.quoteText);
+      $(".name").html("- " + json.quoteAuthor);
     });
+  }
+
+  function tweetQuote() {
+    var tweet = quoteText + " - " + quoteAuthor;
+    // truncate if quote is bigger than twitter's limit
+    if(tweet.length >= TWEET_CHAR_LIMIT) {
+      var truncate = "... -"
+      tweet = tweet.slice(0, (tweetLimit - quoteAuthor.length - truncate.length));
+      tweet = tweet + truncate + quoteAuthor;
+    }
+    var tweetUrl = "http://twitter.com/home?status=" + tweet;
+    window.open(tweetUrl, '_blank');
   }
 
   // after page load, change the quote
@@ -29,5 +48,8 @@ $(document).ready(function() {
   // get a new quote when the button is pressed
   $("#getQuote").on("click", function() {
     newQuote();
+  });
+  $("#tweet").on("click", function() {
+    tweetQuote();
   });
 });
