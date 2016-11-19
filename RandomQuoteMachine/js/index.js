e562622b87690af437ac6791eb0c66a0718a2a3f
David Pierce <pierceda@gmail.com>
2016-11-18 22:59:58 -0800

    Refactor RandomQuoteMachine

--- RandomQuoteMachine/js/index.js
+++ RandomQuoteMachine/js/index.js
@@ -1,36 +1,51 @@
 $(document).ready(function() {
   // global variables
   var TWEET_CHAR_LIMIT = 140;
-  var quoteText = "Somewhere, something incredible is waiting to be known.";
-  var quoteAuthor = "Carl Sagan";
+  var quote = {
+    "author": "Carl Sagan",
+    "text": "Somewhere, something incredible is waiting to be known."
+  };
 
-  // grabs a new quote from a JSON file, compares to avoid current quote
-  function newQuote() {
+  // api call for quote data
+  function getQuote() {
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
-      // remove all Donald quotes--probably actually by his ghostwriter anyway
-      if (json.quoteAuthor == "Donald Trump") {
-        newQuote();
-      }
+
       // make it elegant if no one is attributed
       if (json.quoteAuthor === "") {
         json.quoteAuthor = "Unattributed";
       }
-      quoteText = json.quoteText;
-      quoteAuthor = json.quoteAuthor;
-      showQuote();
+
+      quote.text = json.quoteText;
+      quote.author = json.quoteAuthor;
     });
+    return quote;
   }
 
-  function showQuote(){
-    $(".quote").html(quoteText);
-    $(".attributed").html("- " + quoteAuthor);
+  // grabs a new quote from a JSON file, compares to avoid current quote
+  function newQuote() {
+    freshQuote = getQuote();
+
+    // remove all Donald quotes
+    if (freshQuote.author.indexOf("Donald Trump") != -1) {
+      console.log("***This is not normal***");
+      return -1;
+    }
+
+    showQuote(freshQuote);
+  }
+
+  function showQuote(quote) {
+    $(".quote").html(quote.text);
+    $(".attributed").html("- " + quote.author);
   }
 
   function tweetQuote() {
-    var tweet = quoteText + " - " + quoteAuthor;
+    var quoteText = $(".quote").text();
+    var quoteAuthor = $(".attributed").text();
+    var tweet = quoteText + " " + quoteAuthor;
     // truncate if quote is bigger than twitter's limit
-    if(tweet.length >= TWEET_CHAR_LIMIT) {
-      var truncate = "... -"
+    if (tweet.length >= TWEET_CHAR_LIMIT) {
+      var truncate = "... "
       tweet = tweet.slice(0, (TWEET_CHAR_LIMIT - quoteAuthor.length - truncate.length));
       tweet = tweet + truncate + quoteAuthor;
     }
@@ -39,11 +54,20 @@ $(document).ready(function() {
   }
 
   // at load, show the first quote
-  showQuote();
+  newQuote();
 
   // get a new quote when the button is pressed
   $("#getQuote").on("click", function() {
-    newQuote();
+    function filterQuotes() {
+      if (newQuote() !== -1) {
+
+      } else {
+        setTimeout(function() {
+          newQuote();
+        }, 200);
+      }
+    }
+    filterQuotes();
   });
   $("#tweet").on("click", function() {
     tweetQuote();
